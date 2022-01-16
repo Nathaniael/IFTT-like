e98959f9a0458286d77583483160f8675bbb59d8
Kilian Le Calvez <kilianlecalvez@Kilians-MacBook-Pro.local>
2022-01-16 23:53:51 +0100

    [+] home page animation

--- front_web/src/Components/AppBar.jsx
+++ front_web/src/Components/AppBar.jsx
@@ -4,7 +4,9 @@ import styles from './AppBar.module.css'
 function AppBar() {
     return (
         <div className={styles.appBar}>
-            
+            <div className={styles.navHover}><a>- Login -</a></div>
+            <div className={styles.navHover}><a className={styles.title}>✨ AREA ✨</a></div>
+            <div className={styles.navHover}><a>- Get Started -</a></div>
         </div>
     )
 }
--- front_web/src/Components/Home.jsx
+++ front_web/src/Components/Home.jsx
@@ -2,8 +2,24 @@ import React from 'react'
 import styles from './Home.module.css'
 import AppBar from './AppBar'
 
+function initGrid(squareSize) {
+    const nbPerline = window.outerWidth / squareSize
+    const nbPerColumn = window.outerHeight / squareSize
+    
+    var newGrid = []
+    for (var i = 0; i < nbPerColumn; i++) {
+        var line = []
+        for (var j = 0; j < nbPerline; j++) {
+            line = line.concat({top: i * squareSize + "px", left: j * squareSize + "px", width: squareSize + "px", height: squareSize + "px"})
+        }
+        newGrid = newGrid.concat(line)
+    }
+    return newGrid
+}
+
 function Home() {
     const [pos, setPos] = React.useState({left: -100 + "px", top: -100 + "px"})
+    const [grid, setGrid] = React.useState(initGrid(100))
 
     function moveTheMouse(e) {
         setPos({ left: e.clientX + "px", top: e.clientY + "px"})
@@ -11,11 +27,16 @@ function Home() {
 
     return (
         <div className={styles.home} onMouseMove={(e) => {moveTheMouse(e)}}>
-            <img className={styles.backgroundHomeImg} src="backgroundHomeImg.jpg"></img>
             <AppBar></AppBar>
             <button className={styles.buttonArea}>Get Area Started</button>
             <div className={styles.followMouse} style={pos}></div>
-            <div>{pos.x}{pos.y}</div>
+            <div className={styles.grid}>
+                {grid?.map((elem, index) => {
+                    return (
+                        <div className={styles.gridItem} key={index} style={elem}></div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
