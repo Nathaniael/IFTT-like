e9322a1e6cd95acee7ced170c15264513043cca0
Kilian Le Calvez <kilianlecalvez@Kilians-MacBook-Pro.local>
2022-02-08 22:32:38 +0100

    [-] node modules

--- client_web/src/App.js
+++ client_web/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Home from './Components/Home';
+import Login from './Components/Login';
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <Login></Login>
     </div>
   );
 }
