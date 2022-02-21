0eb741c23f658d8e9247a69d0676f28f7639613d
Baptdess <baptiste.dessert@epitech.eu>
2022-02-21 18:31:17 +0100

    [+] Css service page

--- client_web/src/Components/Services/Service.jsx
+++ client_web/src/Components/Services/Service.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import styles from './ChooseService.module.css'
+import React, { useState }from 'react'
+import styles from './Service.module.css'
 
 const service = {
       "id": 1,
@@ -18,39 +18,47 @@ const service = {
       ]
 }
 
-function Service({}) {
-  return (
-    <div className={styles.servicePage}>
-      <div className={styles.subDescription}>
-        <div className={styles.littleTitle}>
-            Actions
+function ButtonService ({ elem }) {
+    const [clicked, setClicked] = useState(false)
+    return (
+        <div className={styles.buttonPadding}>
+            <button className={`${styles.button} ${clicked ? styles.clicked : null}`} onClick={() => !clicked ? setClicked(true) : setClicked(false)}>{elem}</button>
         </div>
-        <div>
-            {service?.action?.map((elem, index) => {
-              return (
-                  <div key={index} className={styles.littleElem}>
-                      {elem}
-                  </div>
-              )
-          })}
+    )
+}
+
+function Service() {
+    const [hover, setHover] = React.useState(false)
+    return (
+        <div className={styles.servicePage}>
+            <div className={styles.subDescriptionAction}>
+                <div className={styles.littleTitle}>
+                    Actions
+                </div>
+                <div>
+                    {service?.action?.map((elem, index) => {
+                        return (
+                              <ButtonService key={index} elem={elem}></ButtonService> 
+                        )
+                    })}
+                </div>
+            </div>
+            <img onMouseEnter={() => {setHover(true)}} onMouseLeave={() => {setHover(false)}} className={`${styles.serviceLogo} ${hover ? styles.logoGoCenter : null}`} src={service?.imgUrl} alt={service?.imgUrl}></img>
+            <div className={styles.subDescriptionReAction}>
+                <div className={styles.littleTitle}>
+                    ReActions
+                </div>
+                <div>
+                    {service?.reaction?.map((elem, index) => {
+                        return (
+                            <div key={index}>
+                                <ButtonService key={index} elem={elem}></ButtonService>
+                            </div>
+                        )
+                    })}
+                </div> 
+            </div>
         </div>
-        </div>
-        <img src={service?.imgUrl} alt={service?.imgUrl} className={styles.logoGoCenter}></img>
-        <div className={styles.subDescription}>
-        <div className={styles.littleTitle}>
-            ReActions
-        </div>
-        <div>
-            {service?.reaction?.map((elem, index) => {
-              return (
-                  <div key={index} className={styles.littleElem}>
-                      {elem}
-                  </div>
-              )
-          })}
-        </div>
-        </div>
-    </div>
   )
 }
 
