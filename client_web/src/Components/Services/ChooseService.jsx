4d21b99f35c1ee5a5928d0efc1f837dc8d6c18c2
Baptdess <baptiste.dessert@epitech.eu>
2022-02-21 12:10:55 +0100

    [+] Sevice page add css

--- client_web/src/Components/Services/ChooseService.jsx
+++ client_web/src/Components/Services/ChooseService.jsx
@@ -5,63 +5,182 @@ const listServices = [
     {
         "id": 1,
         "imgUrl": "three/chrome.png",
-        "description": ""
+        "action": [
+            "See Post",
+            "Retweet",
+            "Fav Something",
+            "Receive Message"
+        ],
+        "reaction": [
+            "Post",
+            "Retweet",
+            "Fav Something",
+            "Send Message"
+        ]
     },
     {
         "id": 2,
-        "imgUrl": "three/clashroyale.png"
+        "imgUrl": "three/clashroyale.png",
+        "action": [
+            "See Post",
+            "Retweet",
+            "Fav Something",
+            "Receive Message"
+        ],
+        "reaction": [
+            "Post",
+            "Retweet",
+            "Fav Something",
+            "Send Message"
+        ]
     },
     {
         "id": 3,
-        "imgUrl": "three/discord.png"
+        "imgUrl": "three/discord.png",
+        "action": [
+            "See Post",
+            "Retweet",
+            "Fav Something",
+            "Receive Message"
+        ],
+        "reaction": [
+            "Post",
+            "Retweet",
+            "Fav Something",
+            "Send Message"
+        ]
     },
     {
         "id": 4,
-        "imgUrl": "three/facebook.png"
+        "imgUrl": "three/facebook.png",
+        "action": [
+            "See Post",
+            "Retweet",
+            "Fav Something",
+            "Receive Message"
+        ],
+        "reaction": [
+            "Post",
+            "Retweet",
+            "Fav Something",
+            "Send Message"
+        ]
     },
     {
         "id": 5,
-        "imgUrl": "three/fortnite.png"
+        "imgUrl": "three/fortnite.png",
+        "action": [
+            "See Post",
+            "Retweet",
+            "Fav Something",
+            "Receive Message"
+        ],
+        "reaction": [
+            "Post",
+            "Retweet",
+            "Fav Something",
+            "Send Message"
+        ]
     },
     {
         "id": 6,
-        "imgUrl": "three/reddit.png"
+        "imgUrl": "three/reddit.png",
+        "action": [
+            "See Post",
+            "Retweet",
+            "Fav Something",
+            "Receive Message"
+        ],
+        "reaction": [
+            "Post",
+            "Retweet",
+            "Fav Something",
+            "Send Message"
+        ]
     },
     {
         "id": 7,
-        "imgUrl": "three/tft.png"
+        "imgUrl": "three/tft.png",
+        "action": [
+            "See Post",
+            "Retweet",
+            "Fav Something",
+            "Receive Message"
+        ],
+        "reaction": [
+            "Post",
+            "Retweet",
+            "Fav Something",
+            "Send Message"
+        ]
     },
     {
         "id": 8,
-        "imgUrl": "three/twitter.png"
+        "imgUrl": "three/twitter.png",
+        "action": [
+            "See Post",
+            "Retweet",
+            "Fav Something",
+            "Receive Message"
+        ],
+        "reaction": [
+            "Post",
+            "Retweet",
+            "Fav Something",
+            "Send Message"
+        ]
     },
 ]
 
-function DescriptionWidget({ hidden, onLeft }) {
+function SubDescription({ title, list }) {
     return (
-        <div className={`${styles.descriptionService} ${hidden ? styles.hidden : null} ${onLeft ? styles.descriptionLeft : styles.descriptionRight}`}>BLABLA<br/>BLABLA<br/></div>
+        <div className={styles.subDescription}>
+            <div className={styles.littleTitle}>
+                {title}
+            </div>
+            <div>
+            {list?.map((elem, index) => {
+                return (
+                    <div key={index} className={styles.littleElem}>
+                        {elem}
+                    </div>
+                )
+            })}
+            </div>
+        </div>
     )
 }
 
-function ServiceWidget({ imgUrl, onLeft }) {
+function DescriptionWidget({ hidden, onLeft, action, reaction }) {
+    return (
+        <div className={`${styles.descriptionService} ${hidden ? styles.hidden : null} ${onLeft ? styles.descriptionLeft : styles.descriptionRight}`}>
+            <SubDescription title="Actions" list={action}></SubDescription>
+            <SubDescription title="ReActions" list={reaction}></SubDescription>
+        </div>
+    )
+}
+
+function ServiceWidget({ service, onLeft }) {
     const [clicked, setClicked] = React.useState(false)
 
     return (
         <div>
-            <DescriptionWidget hidden={!clicked} onLeft={onLeft}></DescriptionWidget>    
-            <img onMouseEnter={() => {setClicked(true)}} onMouseLeave={() => {setClicked(false)}} className={`${styles.serviceLogo} ${clicked ? styles.logoGoCenter : null}`} src={imgUrl} alt={imgUrl}></img>
+            <DescriptionWidget hidden={!clicked} onLeft={onLeft} action={service?.action} reaction={service?.reaction}></DescriptionWidget>    
+            <img onMouseEnter={() => {setClicked(true)}} onMouseLeave={() => {setClicked(false)}} className={`${styles.serviceLogo} ${clicked ? styles.logoGoCenter : null}`} src={service?.imgUrl} alt={service?.imgUrl}></img>
         </div>
     )
 }
 
 function ChooseService() {
   return (
-    <div className={styles.listServices}>
-        {listServices?.map((elem, index) => {
-            return (
-                <ServiceWidget key={elem?.id} imgUrl={elem?.imgUrl} onLeft={index % 2 == 0 ? true : false}></ServiceWidget>
-            )
-        })}
+    <div className={styles.servicePage}>
+        <div className={styles.listServices}>
+            {listServices?.map((elem, index) => {
+                return (
+                    <ServiceWidget key={elem?.id} service={elem} onLeft={index % 2 == 0 ? true : false}></ServiceWidget>
+                )
+            })}
+        </div>
     </div>
   )
 }
