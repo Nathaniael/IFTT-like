763523e51284b1ecdabae5d2e4653bdea40badf3
Kilian Le Calvez <kilianlecalvez@kilians-macbook-pro.home>
2022-02-23 18:19:25 +0100

    [+] setup, this is area

---
+++ client_mobile/lib/Services/ServicesPage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ServicesPage extends StatefulWidget {
+  const ServicesPage({Key? key}) : super(key: key);
+
+  @override
+  State<ServicesPage> createState() => _ServicesPageState();
+}
+
+class _ServicesPageState extends State<ServicesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Image.asset("web/png/emile.png"),
+    );
+  }
+}
---
+++ client_mobile/lib/Widgets/Text/TextADN.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TextANM extends StatelessWidget {
+  final String text;
+
+  const TextANM(this.text, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      textAlign: TextAlign.center,
+      style: const TextStyle(
+          color: Colors.white, fontSize: 40, fontFamily: 'AvenirNext'),
+    );
+  }
+}
