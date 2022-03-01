4d184fa126248d2d6aae65812a8d8b48f94a9233
Nathaniael TRISTRAM <nathaniael.tristram@epitech.eu>
2022-03-01 19:10:53 +0100

    [skip ci] change cors origin

--- back/src/main.ts
+++ back/src/main.ts
@@ -5,7 +5,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:8081, http://pantharea.fun:8081',
+    origin: ['http://localhost:8081','http://pantharea.fun:8081'],
     credentials: true,
     allowedHeaders:'Set-Cookie, content-type, X-Hub-Signature'});
   app.use(cookieParser());
