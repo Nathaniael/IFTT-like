5ca69e86cd0d0c83a029eabfc140ff687133effc
Kilian Le Calvez <kilianlecalvez@Kilians-MacBook-Pro.local>
2022-03-05 18:03:08 +0100

    [+] register flutter

--- client_mobile/lib/apiprovider.dart
+++ client_mobile/lib/apiprovider.dart
@@ -1,5 +1,16 @@
 import 'dart:convert';
+import 'dart:io';
 import 'package:http/http.dart' as http;
+
+enum Status { success, error }
+
+class Response {
+  Status status;
+  String? message;
+  Map<String, dynamic>? data;
+
+  Response({required this.status, this.data, this.message});
+}
 
 class Session {
   static final Session _session = Session._internal();
@@ -11,22 +22,43 @@ class Session {
 
   Map<String, String> headers = {};
 
-  Future<Map> get(Uri url) async {
-    http.Response response = await http.get(url, headers: headers);
-    updateCookie(response);
-    return json.decode(response.body);
-  }
-
-  Future<Map> post(Uri url, dynamic data) async {
-    http.Response response = await http.post(url, body: data, headers: headers);
-    updateCookie(response);
-    return json.decode(response.body);
-  }
-
   void updateCookie(http.Response response) {
     String rawCookie = response.headers['set-cookie'].toString();
     int index = rawCookie.indexOf(';');
     headers['cookie'] =
         (index == -1) ? rawCookie : rawCookie.substring(0, index);
+  }
+
+  Future<Response> get(Uri url) async {
+    http.Response response;
+    try {
+      response = await http.get(url, headers: headers);
+    } on SocketException {
+      return Response(status: Status.error, message: "Unexpected error");
+    }
+    return Response(status: Status.success, data: json.decode(response.body));
+  }
+
+  Future<Response> post(Uri url, dynamic body) async {
+    http.Response response;
+    print(body.toJson());
+    try {
+      response = await http.post(url, headers: headers, body: body.toJson());
+    } on Exception {
+      return Response(status: Status.error, message: "Unexpected error");
+    }
+    return Response(status: Status.success, data: json.decode(response.body));
+  }
+
+  Future<Map> getWithCookies(Uri url) async {
+    http.Response response = await http.get(url, headers: headers);
+    updateCookie(response);
+    return json.decode(response.body);
+  }
+
+  Future<Map> postWithCookies(Uri url, dynamic data) async {
+    http.Response response = await http.post(url, body: data, headers: headers);
+    updateCookie(response);
+    return json.decode(response.body);
   }
 }
